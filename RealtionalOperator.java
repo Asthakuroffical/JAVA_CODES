b662a15928b2ef2084defed9f2018e662d8be69c
Asthakuroffical <122505940+Asthakuroffical@users.noreply.github.com>
2025-01-28 23:30:58 +0530

    Add files via upload

---
+++ RealtionalOperator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class RealtionalOperator{
+public static void main (String[] args) {
+  int a=10;
+  int b = 5;
+  System.out.println("equal Realtional Operator " + (a==b));
+  System.out.println("not equal Relational Operator "+ (a!=b));
+  System.out.println("Greater than Operator " + (a>b));
+  System.out.println("Greater than Equal to " + (a>=b));
+  System.out.println("Less Than Equal to(Relational Operator) "+(a<=b));
+}}
